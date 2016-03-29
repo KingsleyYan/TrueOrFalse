15f6d8b8dc64f0917c36521603d4080c1ef42d2f
King Yan <mcse2003.yan@gmail.com>
2016-03-29 18:01:46 +0800

    update

--- app/src/main/java/com/kingyan/android/trueorfalse/MainActivity.java
+++ app/src/main/java/com/kingyan/android/trueorfalse/MainActivity.java
@@ -1,53 +1,122 @@
 package com.kingyan.android.trueorfalse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final String INDEX_KEY = "index";
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
+    private Button mCheatButton;
     private TextView mQuetionText;
     private int mCurrentIndex = 0;
     private Question[] questions = new Question[]{
-            new Question(R.string.question_direction, false),
-            new Question(R.string.question_today, true)
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle savedInstanceState) called");
         setContentView(R.layout.activity_main);
+
         mQuetionText = (TextView) findViewById(R.id.question_text_view);
-        final int question = questions[mCurrentIndex].getQuestion();
-        mQuetionText.setText(question);
+        mQuetionText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (++mCurrentIndex) % questions.length;
+                updateQuestion();
+            }
+        });
+
         mNextButton = (Button) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex + 1) % questions.length;
-                int question = questions[mCurrentIndex].getQuestion();
-                mQuetionText.setText(question);
+                mCurrentIndex = (++mCurrentIndex) % questions.length;
+                Log.d(TAG, String.valueOf(mCurrentIndex));
+                updateQuestion();
             }
         });
+
+
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (--mCurrentIndex + questions.length) % questions.length;
+                Log.d(TAG, String.valueOf(mCurrentIndex));
+                updateQuestion();
+            }
+        });
+
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CheatActivity.class);
+                boolean mTrueQuestion = questions[mCurrentIndex].isTrueQuestion();
+                intent.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, mTrueQuestion);
+                startActivity(intent);
+            }
+        });
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(INDEX_KEY, 0);
+        }
+
+        updateQuestion();
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                answerQuestion(true);
             }
         });
+
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                answerQuestion(false);
             }
         });
+    }
+
+    private void updateQuestion() {
+        int question = questions[mCurrentIndex].getQuestion();
+        mQuetionText.setText(question);
+    }
+
+    private void answerQuestion(boolean userPress) {
+        boolean answerIsTrue = questions[mCurrentIndex].isTrueQuestion();
+        int responseResId;
+        if (answerIsTrue == userPress) {
+            responseResId = R.string.correct_toast;
+        } else {
+            responseResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, responseResId, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState called");
+        outState.putInt(INDEX_KEY, mCurrentIndex);
     }
 }
